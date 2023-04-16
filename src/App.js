e5f7d3a7e97d1a8ea059be2b837c4f8e36d76278
GoktugYmsk <gogtug033@outlook.com>
2023-04-16 10:02:15 +0300

    master template prepared

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [user, setUser] = useState('')
+  const [password, setPassword] = useState('')
+
+  const inputSuccess = () => {
+    if (user === 'admin' && password === '1233') {
+      console.log('çalıştı')
+    }
+    else {
+      console.log('Hatalı kullanıcı adı veya şifre')
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
       </header>
-
+      <div className='login' >
+        <input onChange={e => setUser(e.target.value)} placeholder='Kullanıcı adı' />
+        <input onChange={e => setPassword(e.target.value)} placeholder='Şifre' />
+      </div>
+      <button onClick={inputSuccess} className='loginButton' >Giriş</button>
     </div>
   );
 }
