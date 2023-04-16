ada215b19c23733f8218d74af471c522b19299ce
GoktugYmsk <gogtug033@outlook.com>
2023-04-16 11:30:14 +0300

    codes transferred to login page

--- src/App.js
+++ src/App.js
@@ -1,34 +1,38 @@
 import { useState } from 'react';
+import { Router, useHistory } from 'react-router-dom';
 
+import Home from './components/homePage';
 import './App.css';
 
 function App() {
-  const [user, setUser] = useState('')
-  const [password, setPassword] = useState('')
-  const [token, setToken] = useState('')
+  const history = useHistory();
+  const [user, setUser] = useState('');
+  const [password, setPassword] = useState('');
+  const [token, setToken] = useState('');
+
 
   const inputSuccess = () => {
     if (user === 'admin' && password === '1233') {
-      const newToken = 'example-token' 
-      setToken(newToken)
-      localStorage.setItem('token', newToken) 
-      console.log('Token oluşturuldu ve localStorage\'e kaydedildi.')
+      const newToken = 'example-token';
+      setToken(newToken);
+      localStorage.setItem('token', newToken);
+      console.log('Token oluşturuldu ve localStorage\'e kaydedildi.');
+      history.push('/home');
+    } else {
+      console.log('Hatalı kullanıcı adı veya şifre');
     }
-    else {
-      console.log('Hatalı kullanıcı adı veya şifre')
-    }
-  }
+  };
 
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-      <div className='login' >
-        <input onChange={e => setUser(e.target.value)} placeholder='Kullanıcı adı' />
-        <input onChange={e => setPassword(e.target.value)} placeholder='Şifre' />
-        <button onClick={inputSuccess} className='loginButton' >Giriş</button>
+      <div className="App">
+        <header className="App-header">
+        </header>
+        <div className='login' >
+          <input onChange={e => setUser(e.target.value)} placeholder='Kullanıcı adı' />
+          <input onChange={e => setPassword(e.target.value)} placeholder='Şifre' />
+          <button onClick={inputSuccess} className='loginButton' >Giriş</button>
+        </div>
       </div>
-    </div>
   );
 }
 
