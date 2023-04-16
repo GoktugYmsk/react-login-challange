79236c329c5866e2fed1a387e2c006b681c26325
GoktugYmsk <gogtug033@outlook.com>
2023-04-16 10:10:08 +0300

    test code written

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import { useState } from 'react';
+
 import './App.css';
 
 function App() {
   const [user, setUser] = useState('')
   const [password, setPassword] = useState('')
+  const [token, setToken] = useState('')
 
   const inputSuccess = () => {
     if (user === 'admin' && password === '1233') {
-      console.log('çalıştı')
+      const newToken = 'example-token' 
+      setToken(newToken)
+      localStorage.setItem('token', newToken) 
+      console.log('Token oluşturuldu ve localStorage\'e kaydedildi.')
     }
     else {
       console.log('Hatalı kullanıcı adı veya şifre')
@@ -21,8 +26,8 @@ function App() {
       <div className='login' >
         <input onChange={e => setUser(e.target.value)} placeholder='Kullanıcı adı' />
         <input onChange={e => setPassword(e.target.value)} placeholder='Şifre' />
+        <button onClick={inputSuccess} className='loginButton' >Giriş</button>
       </div>
-      <button onClick={inputSuccess} className='loginButton' >Giriş</button>
     </div>
   );
 }
