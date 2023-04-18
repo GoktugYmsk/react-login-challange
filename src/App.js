0f3fbd9d4ad96ad0f7b671220e246ddab660ef2a
GoktugYmsk <gogtug033@outlook.com>
2023-04-18 11:36:09 +0300

    app  login button added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h3>Hoşgeldiniz</h3>
+      <br/>
+      <p>Giriş yapmak için butona tıklayın</p>
+      <button className='appLoginButton'>Devam et</button>
       <Routes>
         <Route path='/login' element={<Login />} />
         <Route path='/home' element={<Home />} />
