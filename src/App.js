ae784755e3ed9fecd3541ab6f9a51f4b4f82221a
GoktugYmsk <gogtug033@outlook.com>
2023-04-18 11:50:05 +0300

    splash screen created

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-import { NavLink, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 import Home from './components/homePage';
 import Login from './components/loginPage/Login';
 import './App.css';
 
 function App() {
+  const navigate = useNavigate();
+  const [hideEntrance, setHideEntrance] = useState(false);
+
+  const openLoginScreen = () => {
+    setHideEntrance(true);
+    navigate('/login');
+  };
+
   return (
     <div className="App">
-      <h3>Hoşgeldiniz</h3>
-      <br/>
-      <p>Giriş yapmak için butona tıklayın</p>
-      <button className='appLoginButton'>Devam et</button>
+      {!hideEntrance && (
+        <div className='entrance' >
+          <h3>Hoşgeldiniz</h3>
+          <br />
+          <p>Giriş yapmak için butona tıklayın</p>
+          <button onClick={openLoginScreen} className='appLoginButton'>Devam et</button>
+        </div>
+      )}
       <Routes>
         <Route path='/login' element={<Login />} />
         <Route path='/home' element={<Home />} />
