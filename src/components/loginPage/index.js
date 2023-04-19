70e02c4bce8f29e3f0717bf63f23ac785a70631d
GoktugYmsk <gogtug033@outlook.com>
2023-04-19 10:14:06 +0300

    debug fixed

--- src/components/loginPage/index.js
+++ src/components/loginPage/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Helmet } from 'react-helmet';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
@@ -13,18 +13,52 @@ function Login() {
   const navigate = useNavigate();
 
   const inputSuccess = () => {
-    axios.post('/api/login', { username: user, password: password })
-      .then(response => {
-        const newToken = response.data.token;
-        setToken(newToken);
-        localStorage.setItem('token', newToken);
-        console.log('Token oluşturuldu ve localStorage\'e kaydedildi.');
-        navigate('/home');
-      })
-      .catch(error => {
-        setError('Hatalı kullanıcı adı veya şifre');
-      });
+    if (user === 'admin' && password === '1234') {
+      const newToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
+      setToken(newToken);
+      localStorage.setItem('token', newToken);
+      console.log('Token oluşturuldu ve localStorage\'e kaydedildi.');
+      navigate('/home');
+    } else {
+      setError('Hatalı kullanıcı adı veya şifre');
+    }
   };
+
+  useEffect(() => {
+    const storedToken = localStorage.getItem('token');
+    if (storedToken) {
+      setToken(storedToken);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (token) {
+      axios.get('/api/login', { headers: { Authorization: `Bearer ${token}` } })
+        .then(response => {
+          console.log('Giriş başarılı');
+        })
+        .catch(error => {
+          console.log('Hata oluştu: ', error.response.status);
+          if (error.response.status === 404) {
+            setError('Giriş başarısız');
+          } else {
+            console.log('Bilinmeyen bir hata oluştu.');
+          }
+        });
+    }
+  }, [token]);
+  
+
+  useEffect(() => {
+    const clearLocalStorage = () => {
+      localStorage.clear();
+      console.log('localStorage temizlendi.');
+    };
+    window.addEventListener('beforeunload', clearLocalStorage);
+    return () => {
+      window.removeEventListener('beforeunload', clearLocalStorage);
+    };
+  }, []);
 
   return (
     <div className="loginMainPage">
@@ -47,59 +81,3 @@ function Login() {
 }
 
 export default Login;
-
-
-
-/*
-
-Asıl Kod bloğu
-
-****************************************************************************************
-
-
-import React, { useState } from 'react';
-import { Helmet } from 'react-helmet';
-import { useNavigate } from 'react-router-dom';
-
-import './index.css'
-
-function Login() {
-  const [user, setUser] = useState('')
-  const [password, setPassword] = useState('')
-  const [token, setToken] = useState('')
-  const navigate = useNavigate()
-
-  const inputSuccess = () => {
-    if (user === 'admin' && password === '1234') {
-      const newToken = 'exampleToken';
-      setToken(newToken);
-      localStorage.setItem('token', newToken);
-      console.log('Token oluşturuldu ve localStorage\'e kaydedildi.');
-      navigate('/home');
-    } else {
-      console.log('Hatalı kullanıcı adı veya şifre');
-    }
-  };
-
-  return (
-    <div className="loginMainPage">
-      <Helmet>
-        <title> Login Sayfası </title>
-      </Helmet>
-      <div className='login' >
-        <input onChange={e => setUser(e.target.value)} placeholder='Kullanıcı adı' />
-        <input type='password' onChange={e => setPassword(e.target.value)} placeholder='Şifre' />
-        <button onClick={inputSuccess} className='loginButton' >Giriş</button>
-      </div>
-      {token && (
-        <div className='loginToken' >Kod: {token}</div>
-      )}
-    </div>
-  );
-}
-export default Login;
-
-
-
-
-*/
