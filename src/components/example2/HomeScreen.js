f40a359e61ff386047f8565d14ef0217ae3ccd1d
GoktugYmsk <gogtug033@outlook.com>
2023-04-27 14:48:42 +0300

    attempt to use tokens

--- src/components/example2/HomeScreen.js
+++ src/components/example2/HomeScreen.js
@@ -1,11 +1,14 @@
 import React from 'react'
+
 import { Helmet } from 'react-helmet'
+
+import './home.css'
 
 function HomeScreen() {
   return (
     <div>
       <Helmet>Home Screen Sayfası</Helmet>
-      <h1>Home Screen sayfasına hoşgeldiniz</h1>
+      <h1 className='homeh1' >Home Screen sayfasına hoşgeldiniz</h1>
     </div>
   )
 }
--- src/components/example2/LoginScreen.js
+++ src/components/example2/LoginScreen.js
@@ -1,29 +1,47 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
+import axios from 'axios';
 import { Helmet } from 'react-helmet';
 import { useNavigate } from 'react-router-dom';
 
 import './index.css'
 
 function LoginScreen() {
-    const [input,setInput]= useState('')
-    const [pass,setPass] = useState('')
-    const [saveToken,setSaveToken] = useState('')
+    const [input, setInput] = useState('')
+    const [pass, setPass] = useState('')
+    const [saveToken, setSaveToken] = useState('')
     const navigate = useNavigate()
 
     const handleClick = () => {
-        if(!input || !pass){
+        if (!input || !pass) {
             console.log('Bu alanların Doldurulması zorunludur')
         }
-        else if (input === 'admin' && pass === '1234')
-        {
+        else if (input === 'admin' && pass === '1234') {
             const tokenAdd = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
             setSaveToken(tokenAdd)
-            localStorage.setItem('saveToken',tokenAdd)
+            localStorage.setItem('saveToken', tokenAdd)
             console.log('Token localeStorag`e kaydedildi ')
             navigate('homeScreen')
         }
     }
+
+
+    useEffect(() => {
+        const config = { headers: { "saveToken": localStorage.getItem('saveToken') } }
+
+        axios
+            .get('https://mock-api-service.vercel.app/discoverFirstHorizontalList',
+                config
+            )
+            .then((response) => {
+                if (response.status < 201) {
+                    console.log('network 200\'ün altında başarılı bir şekilde çalışıyor');
+                }
+            })
+            .catch((error) => {
+                console.log('Hata oluştu: ', error);
+            });
+    }, []);
 
     return (
         <div className='screenContainer' >
